2cc5d6b19de3e4e095bb1b14f2e40c32a7087d92
abhijithpr08 <abhith628@gmail.com>
2026-01-12 12:16:04 +0530

    fix callback error

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,9 +1,5 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 passport.use(
   new GoogleStrategy(
@@ -13,17 +9,7 @@ passport.use(
       callbackURL: "http://localhost:5000/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      const user = {
-        googleId: profile.id,
-        name: profile.displayName,
-        email: profile.emails[0].value,
-      };
-
-      const token = jwt.sign(user, process.env.JWT_SECRET, {
-        expiresIn: "1h",
-      });
-
-      done(null, { user, token });
+      return done(null, profile);
     }
   )
 );
--- server/index.js
+++ server/index.js
@@ -1,27 +1,41 @@
+import "./env.js";
+
 import express from "express";
 import cors from "cors";
 import passport from "passport";
-
 import "./config/passport.js";
-import authRoutes from "./routes/auth.js";
-
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.use(passport.initialize());
 
-app.get("/test", (req, res) => {
-  res.send("Backend reachable");
-});
-
-
-app.use("/auth", authRoutes);
-
 app.get("/", (req, res) => {
-  res.send("Backend Running");
+  res.send("Server running");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    session: false,
+  }),
+  (req, res) => {
+    res.json({
+      success: true,
+      user: req.user,
+    });
+  }
+);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
